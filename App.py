20294707947a7749936f4caca15a0e6e2200da0c
KarinaParraS <117685691+KarinaParraS@users.noreply.github.com>
2022-11-18 13:31:35 -0600

    Update App.py

--- App.py
+++ App.py
@@ -82,8 +82,8 @@ import pandas as pd
 import numpy as np
 
 df = pd.DataFrame(
-   np.random.randn(10, 5),
-   columns=('col %d' % i for i in range(5)))
+   np.random.randn(3, 5),
+   columns=('col %d' % i for i in range(4)))
 
 st.table(df)
 
